05a6e7023f63b647f85bf4b3bcd43f82ad123f04
Gleiston Machado <gleiston.dev@gmail.com>
2021-08-06 14:18:46 -0300

    Projeto inicial

---
+++ src/main/java/com/gleiston/springboot/SpringbootApplication.java
@@ -0,0 +1,21 @@
+package com.gleiston.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EntityScan(basePackages = "com.gleiston.springboot.model")
+@ComponentScan(basePackages = {"com.*"})
+@EnableJpaRepositories(basePackages = {"com.gleiston.springboot.repository"})
+@EnableTransactionManagement
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gleiston/springboot/model/Person.java
@@ -0,0 +1,64 @@
+package com.gleiston.springboot.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Person implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@NotEmpty(message = "Can not be empty")
+	private String name;
+	
+	@NotEmpty(message = "Can not be empty")
+	private String email;
+	
+	public Person() {
+	}
+	
+	public Person(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
